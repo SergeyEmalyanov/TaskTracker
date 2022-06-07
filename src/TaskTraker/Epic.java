0a5d43912e172677bfde4f9407afdfa39ac0ee8c
SergeyEmelyanov <sergej-rabota@yandex.ru>
2022-06-07 23:11:00 +0300

    feat:перенос метода checkAndUpdateEpic в клас Epic

--- src/TaskTraker/Epic.java
+++ src/TaskTraker/Epic.java
@@ -11,20 +11,21 @@ class Epic extends Task {
     }
 
 
-
     protected Epic EpicUpdate(StatusOfTasks statusOfTasks) {
-        this.statusOfTasks=statusOfTasks;
+        this.statusOfTasks = statusOfTasks;
         return this;
     }
 
-    protected Epic addSubTask (int idSubTask){
+    protected Epic addSubTask(int idSubTask) {
         this.idSubTask.add(idSubTask);
         return this;
     }
 
-    protected ArrayList <Integer> getIdSubTask (){
+    protected ArrayList<Integer> getIdSubTask() {
         return this.idSubTask;
     }
+
+
    /* protected Epic calculationEpicStatus(){
         for (Integer id : idSubTask){
             if (TasksManager.subTasks.get(id).getStatusOfTasks()==StatusOfTasks.IN_PROGRESS){
